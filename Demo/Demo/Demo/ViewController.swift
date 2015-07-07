0c8b2149317bcd829c732260ab8684a5d4f35d9c
Pham Ba Tho <batho@ssf.vn>
2015-07-07 23:14:29 +0700

    Initial Commit

---
+++ Demo/Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Pham Ba Tho on 7/7/15.
+//  Copyright (c) 2015 PHAM BA THO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
