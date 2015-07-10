c43dfdcfa1b2286dcf4c2a0fcd40b54e805871ab
Pham Ba Tho <batho@ssf.vn>
2015-07-10 18:33:21 +0700

    Update asset files

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -18,13 +18,13 @@ class ViewController: UIViewController {
         self.selectedCellLabel.text = items.first
         self.navigationController?.navigationBar.translucent = false
         UINavigationBar.appearance().translucent = false
-        self.navigationController?.navigationBar.barTintColor = UIColor(red: 240.0/255.0, green:32.0/255.0, blue:60.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         let menuView = BTNavigationDropdownMenu(frame:  CGRectMake(0.0, 0.0, 300, 44), title: items.first!, items: items, containerView: self.view)
         menuView.cellHeight = 50
-        menuView.cellBackgroundColor = UIColor(red: 240.0/255.0, green:32.0/255.0, blue:60.0/255.0, alpha: 1.0)
-        menuView.cellSelectionColor = UIColor(red: 210.0/255.0, green:32.0/255.0, blue:60.0/255.0, alpha: 1.0)
+        menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
+        menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
         menuView.cellTextLabelColor = UIColor.whiteColor()
         menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
         menuView.arrowPadding = 15
