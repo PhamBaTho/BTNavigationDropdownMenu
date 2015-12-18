e485453ec8b8460bcd35abf4aea2577050a7b2e2
Takuya Okamoto <blackn.red42@gmail.com>
2015-12-18 16:43:13 +0900

    support Carthage

---
+++ Demo/BTNavigationDropdownMenu/BTNavigationDropdownMenu.h
@@ -0,0 +1,19 @@
+//
+//  BTNavigationDropdownMenu.h
+//  BTNavigationDropdownMenu
+//
+//  Created by 岡本拓也 on 2015/12/18.
+//  Copyright © 2015年 PHAM BA THO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT double BTNavigationDropdownMenuVersionNumber;
+
+//! Project version string for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT const unsigned char BTNavigationDropdownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTNavigationDropdownMenu/PublicHeader.h>
+
+
