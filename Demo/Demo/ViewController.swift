dc1965fc3385f219271ee3abd94b36daf8a0c221
Adam Hartford <adam@adamhartford.com>
2016-06-08 00:17:19 -0400

    Fix initial title

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
-        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items[1].title, items: items)
+        menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items[0].title, items: items)
         menuView.cellHeight = 50
         menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
         menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
