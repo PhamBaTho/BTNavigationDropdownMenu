a055671ceabc0a644a4994cf8fb399a348f2baf2
Rishabh Tayal <rtayal11@gmail.com>
2016-08-02 12:17:01 -0500

    Revert "Xcode 8 beta 4 error fix"
    This reverts commit b27710bee340e01d78012b427260b2aa7acefbf6.

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController, BTNavigationDropdownDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.selectedCellLabel.text = items.first
-        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.translucent = false
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.0/255.0, green:180/255.0, blue:220/255.0, alpha: 1.0)
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, containerView: self.navigationController!.view, title: "Dropdown Menu", items: items, menuDelegate: self)
         menuView.shouldChangeTitleText = false
@@ -28,12 +28,12 @@ class ViewController: UIViewController, BTNavigationDropdownDelegate {
         menuView.cellBackgroundColor = self.navigationController?.navigationBar.barTintColor
         menuView.cellSelectionColor = UIColor(red: 0.0/255.0, green:160.0/255.0, blue:195.0/255.0, alpha: 1.0)
         menuView.shouldKeepSelectedCellColor = true
-        menuView.cellTextLabelColor = UIColor.white
+        menuView.cellTextLabelColor = UIColor.whiteColor()
         menuView.cellTextLabelFont = UIFont(name: "Avenir-Heavy", size: 17)
-        menuView.cellTextLabelAlignment = .left // .Center // .Right // .Left
+        menuView.cellTextLabelAlignment = .Left // .Center // .Right // .Left
         menuView.arrowPadding = 15
         menuView.animationDuration = 0.5
-        menuView.maskBackgroundColor = UIColor.black
+        menuView.maskBackgroundColor = UIColor.blackColor()
         menuView.maskBackgroundOpacity = 0.3
         menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
@@ -43,12 +43,12 @@ class ViewController: UIViewController, BTNavigationDropdownDelegate {
         self.navigationItem.titleView = menuView
     }
     
-    func dropdownTableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "cell")
+    func dropdownTableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("cell")
         if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
         }
-        cell?.textLabel?.text = items[(indexPath as NSIndexPath).row]
+        cell?.textLabel?.text = items[indexPath.row]
         cell?.imageView?.image = UIImage(named: "ic_video")
         return cell!
     }
