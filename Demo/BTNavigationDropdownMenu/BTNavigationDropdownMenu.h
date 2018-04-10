0742ad449f9f3eba43c5097f2a1d939b231d7b69
Dominik <dominikbutz@gmail.com>
2018-04-10 17:01:08 +0800

    creating BTDropdownMenu framework

---
+++ Demo/BTNavigationDropdownMenu/BTNavigationDropdownMenu.h
@@ -0,0 +1,19 @@
+//
+//  BTNavigationDropdownMenu.h
+//  BTNavigationDropdownMenu
+//
+//  Created by Dominik Butz on 10.04.18.
+//  Copyright © 2018 PHAM BA THO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT double BTNavigationDropdownMenuVersionNumber;
+
+//! Project version string for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT const unsigned char BTNavigationDropdownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTNavigationDropdownMenu/PublicHeader.h>
+
+
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BTNavigationDropdownMenu
 
 class ViewController: UIViewController {
 
